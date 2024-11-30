6a2171e204f5c4f494c8fe8fa5dee3c1c0cc2083
Vje <vje783next@gmail.com>
2024-11-30 21:10:20 +0100

    First commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,17 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+import * as bootstrap from 'bootstrap';
+import 'bootstrap';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
